f23e29947d654e0e75e59058f11a5f12e3998942
JIeunhuh <entks301@naver.com>
2023-08-23 17:57:52 +0900

    IMAGEURL - DB

--- Data/test2.py
+++ Data/test2.py
@@ -1,44 +1,50 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 import os
 import re
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Happy:1234@localhost:5432/AI_DB'
-db = SQLAlchemy(app)
 
-class ImageLog(db.Model):
-    __tablename__ = 'recycling_img'
-    detect_log_id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date)
-    time = db.Column(db.Time)
-    img_url = db.Column(db.String)
-
-def extract_datetime_from_filename(filename):
-    match = re.match(r'Wimple_(\d{4})_(\d{2})_(\d{2})_(\d{2})_(\d{2})_(\d{2})', filename)
-    if match:
-        year, month, day, hour, minute, second = match.groups()
-        img_datetime = f'{year}-{month}-{day}', f'{hour}:{minute}:{second}'
-        return img_datetime
-    return None
-
-@app.route('/insert_images_to_db')
-def insert_images_to_db():
-    image_folder_path = 'D:\AI Project\Test Data\AI_Result_img'  # 이미지 파일이 있는 폴더 경로 입력
-    image_files = os.listdir(image_folder_path)
-
-    for image_file in image_files:
-        if image_file.startswith('Wimple_'):
-            img_datetime = extract_datetime_from_filename(image_file)
-            if img_datetime:
-                img_url = f'http://localhost:8080/image/{image_file}'  # 이미지 파일의 URL 형식을 만듭니다.
-                
-                # 이미지 정보를 데이터베이스에 삽입
-                new_image = ImageLog(date=img_datetime[0], time=img_datetime[1], img_url=img_url)
-                db.session.add(new_image)
+# DB Connect
+def connectDB():
+    return psycopg2.connect(
+        dbname='AI_resultSet_test',
+        user='postgres',
+        host='localhost',
+        password='tiger',
+        port=5432
+    )
     
-    db.session.commit()
-    return 'Images inserted to database successfully!'
+@app.route('/update_image', methods=["PUT"])
+def update_image():
+    try:
+      image_dir = 'C:\AI_Result_img'  # 이미지 파일이 있는 폴더 경로 입력
 
+      conn = connectDB()
+      cur = conn.cursor()
+      
+      for filename in os.listdir(image_dir):
+          if filename.endswith('.jpg') or filename.endswith('.png'):
+              image_path = os.path.join(image_dir, filename)
+              image_url = f'http://localhost:8080/images/{filename}'
+              
+              # Insert ImageURL to DB
+              cur.execute('update recycling_img set img_url = %s where id = %d', (image_url,))
+    
+            # 
+      conn.commit()
+    #   close DB
+      conn.close()
+      
+      response = {'message': 'Image URLs updated successfully'}
+      return jsonify(response), 200
+    except Exception as e:
+      response = {'message': 'Error occurred', 'error': str(e)}
+      return jsonify(response), 500
+      
+# @app.route('/read_images', methods=['GET'])
+# def readIMG():
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=8080, debug=True)
