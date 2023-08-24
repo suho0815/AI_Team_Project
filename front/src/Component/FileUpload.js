ebbd2307681b24311118ba778d96ea419686f4c0
YeDongVibe <wkwhstladmss@naver.com>
2023-08-24 14:23:44 +0900

    ddd

--- front/src/Component/FileUpload.js
+++ front/src/Component/FileUpload.js
@@ -1,39 +1,47 @@
-import {Div} from './Div'
-import {Icon} from './Icon'
-import React, {useState, useRef, useEffect} from 'react'
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
 
 export const FileUpload = () => {
-  const fileRef = useRef()
-  const [fileName, setFileName] = useState('')
+  const fileRef = useRef();
+  const [fileName, setFileName] = useState('');
 
-  // 업로드 버튼 클릭 시 DB에 import
-  const UploadBtnClicked = () => {
-    const fileName = fileRef.current?.files[0]?.name
-    console.log(fileName)
-  }
+  const handleUpload = async () => {
+    const file = fileRef.current.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
 
-  // 파일 선택 시 파일 이름 가져오기
+    try {
+      const response = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(response.data); // Upload 결과 확인
+    } catch (error) {
+      console.error('Upload error:', error);
+    }
+  };
+
   const handleFileChange = () => {
     if (fileRef.current.files.length > 0) {
-      const selectedFileName = fileRef.current.files[0].name
-      setFileName(selectedFileName)
+      const selectedFileName = fileRef.current.files[0].name;
+      setFileName(selectedFileName);
     } else {
-      setFileName('')
+      setFileName('');
     }
-  }
+  };
 
   return (
-    <Div className="w-full">
+    <div className="w-full">
       <label htmlFor="file" className="flex justify-between w-4/5 p-4 m-auto border border-gray-200">
         {fileName ? fileName : '파일 찾기'}
-        <Icon name="file_open" />
       </label>
       <input type="file" id="file" ref={fileRef} className="hidden" onChange={handleFileChange} />
-      <Div className="flex justify-end mt-4 mr-4">
-        <button className="text-white btn btn-info" onClick={UploadBtnClicked}>
+      <div className="flex justify-end mt-4 mr-4">
+        <button className="text-white btn btn-info" onClick={handleUpload}>
           업로드
         </button>
-      </Div>
-    </Div>
-  )
-}
+      </div>
+    </div>
+  );
+};
